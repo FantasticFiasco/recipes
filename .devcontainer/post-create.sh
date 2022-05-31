038e6d1fb1fc76650f0e3e11a93121c7bbfe38a1
FantasticFiasco <mattias@kindb.org>
2022-05-31 23:24:31 +0200

    wip

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-# Install the version of Bundler.
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
-fi
+# Install the version of Bundler
+cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 
-# If there's a Gemfile, then run `bundle install`
-# It's assumed that the Gemfile will install Jekyll too
-if [ -f Gemfile ]; then
-    bundle install
-fi
+# Bundler will install Jekyll
+bundle install
