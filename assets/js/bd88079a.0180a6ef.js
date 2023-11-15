d6528a268cf86cad24a58ce2ad6f40c6ba4016f0
FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2023-11-15 06:23:08 +0000

    deploy: f77982c2b08da8e38c2baffd0dac4aaf8eac72da

---
+++ assets/js/bd88079a.0180a6ef.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrecept=self.webpackChunkrecept||[]).push([[8706],{4647:e=>{e.exports=JSON.parse('{"label":"usa","permalink":"/recipes/tags/usa","allTagsPath":"/recipes/tags","count":1,"items":[{"id":"vegetariskt/pommes-frites-i-ugn","title":"Pommes frites i ugn","description":"image","permalink":"/recipes/vegetariskt/pommes-frites-i-ugn"}],"unlisted":false}')}}]);
